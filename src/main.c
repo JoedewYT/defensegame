c94fd8ba050d57ca9842261ce16d0141e1642a14
JoedewYT <joeveraart@gmail.com>
2025-01-12 18:04:10 +0100

    added handling of commands, current commands being quit and clear

--- src/main.c
+++ src/main.c
@@ -1,15 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+
+int handle_cmd(char *cmd, bool *running) {
+    if(strcmp(cmd, "quit") == 0) {
+        *running = false;
+    } else if(strcmp(cmd, "clear") == 0) {
+        system("clear");
+        printf("===DEFENSE GAME===\n\n");
+    } else {
+        printf("INVALID COMMAND!\n");
+    }
+    
+    return 0;
+}
 
 int main() {
     bool running = true;
     char cmd[20];
+    system("clear");
+    printf("===DEFENSE GAME===\n\n");
     while(running) {
-        system("clear");
-        printf("===DEFENSE GAME===\n\n");
-        fgets(cmd, 20, stdin);
-        printf("%s", cmd);
+        printf("#: ");
+        scanf("%19s", &cmd);
+        handle_cmd(cmd, &running);
     }
     return 0;
 }
+
