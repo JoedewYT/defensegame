8bd4be81a41b6d42dec18cf479af4b1a52e08313
JoedewYT <joeveraart@gmail.com>
2025-01-12 18:17:42 +0100

    changed commands sligtly, added help command

--- src/main.c
+++ src/main.c
@@ -6,9 +6,13 @@
 int handle_cmd(char *cmd, bool *running) {
     if(strcmp(cmd, "quit") == 0) {
         *running = false;
+        system("clear");
     } else if(strcmp(cmd, "clear") == 0) {
         system("clear");
         printf("===DEFENSE GAME===\n\n");
+    } else if (strcmp(cmd, "help") == 0) {
+        printf("AVAILABLE COMMANDS:\n");
+        printf(" help - displays this screen\n clear - clears the screen\n quit - quits the game\n");
     } else {
         printf("INVALID COMMAND!\n");
     }
